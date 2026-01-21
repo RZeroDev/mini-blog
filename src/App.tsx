a234f87424e7dd57e922f275ca8f6a29f8e15468
RzeroDev <hadicodemaster@gmail.com>
2026-01-21 16:32:36 +0100

    Refactor App component to use a Button UI component instead of static text

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
-  return <div className="text-3xl font-bold underline">Hello World</div>;
+  return (
+    <Button variant="outline">Click me</Button>
+  );
 };
 
 export default App;
