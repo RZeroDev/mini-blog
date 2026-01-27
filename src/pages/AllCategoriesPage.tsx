670b5da654f1f4c2a5d780e833365cf1d65f1e6d
RzeroDev <hadicodemaster@gmail.com>
2026-01-27 18:38:37 +0100

    Remove BlogHeader component from AllCategoriesPage for a cleaner layout

--- src/pages/AllCategoriesPage.tsx
+++ src/pages/AllCategoriesPage.tsx
@@ -27,8 +27,6 @@ const AllCategoriesPage = () => {
 
   return (
     <div className="min-h-screen bg-background">
-      <BlogHeader />
-
       {/* Header */}
       <section className="bg-card border-b border-border">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
