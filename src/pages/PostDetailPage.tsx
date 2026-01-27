c21023483b37b5546a72751d67753834304a92a0
RzeroDev <hadicodemaster@gmail.com>
2026-01-27 18:40:35 +0100

    Remove BlogHeader component from PostDetailPage to streamline layout

--- src/pages/PostDetailPage.tsx
+++ src/pages/PostDetailPage.tsx
@@ -58,7 +58,6 @@ const PostDetailPage = () => {
 
   return (
     <div className="min-h-screen bg-gray-50">
-      <BlogHeader />
 
       {isLoading ? (
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
